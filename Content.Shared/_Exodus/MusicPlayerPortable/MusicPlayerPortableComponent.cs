2fc99f51a76cc8e9a36d7852778b656e6bdf0077
R-R0S <mikemazov@gmail.com>
2026-07-15 19:08:30 +0300

    add player

---
+++ Content.Shared/_Exodus/MusicPlayerPortable/MusicPlayerPortableComponent.cs
@@ -0,0 +1,7 @@
+namespace Content.Shared._Exodus.MusicPlayerPortable;
+
+[RegisterComponent]
+public sealed partial class MusicPlayerPortableComponent : Component
+{
+
+}
---
+++ Content.Shared/_Exodus/MusicPlayerPortable/MusicPlayerPortableSystem.cs
@@ -0,0 +1,6 @@
+namespace Content.Shared._Exodus.MusicPlayerPortable;
+
+public sealed class MusicPlayerPortableSystem
+{
+
+}

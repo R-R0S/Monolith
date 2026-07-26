3b361ef87f41aeb170a52b9dbab8cda37c184321
R-R0S <mikemazov@gmail.com>
2026-07-26 11:27:16 +0300

    maybe 2

--- Content.Shared/_Exodus/MusicPlayerPortable/MusicPlayerPortableSystem.cs
+++ Content.Shared/_Exodus/MusicPlayerPortable/MusicPlayerPortableSystem.cs
@@ -4,7 +4,6 @@ using Content.Shared.PowerCell;
 using Content.Shared.PowerCell.Components;
 using Robust.Shared.Audio.Components;
 using Robust.Shared.Audio.Systems;
-using Robust.Shared.Containers;
 
 namespace Content.Shared._Exodus.MusicPlayerPortable;
 
@@ -12,6 +11,7 @@ public sealed class MusicPlayerPortableSystem : EntitySystem
 {
     [Dependency] private SharedAppearanceSystem _appearanceSystem = default!;
     [Dependency] private SharedAudioSystem _audio = default!;
+    [Dependency] private SharedUserInterfaceSystem _ui = default!;
 
     public override void Initialize()
     {
@@ -24,28 +24,48 @@ public sealed class MusicPlayerPortableSystem : EntitySystem
 
     private void OnPowerCellChanged(Entity<MusicPlayerPortableComponent> ent, ref PowerCellChangedEvent args)
     {
-        if (args.Ejected && TryComp<JukeboxComponent>(ent, out var jukebox))
-        {
-            StopAudio(ent.Owner, jukebox);
-        }
+        if (!args.Ejected || !TryComp<JukeboxComponent>(ent, out var jukebox))
+            return;
+
+        StopAudio(ent.Owner, jukebox);
+        CloseUI(ent.Owner);
     }
 
     private void OnPowerCellSlotEmpty(Entity<MusicPlayerPortableComponent> ent, ref PowerCellSlotEmptyEvent args)
     {
-        if (TryComp<JukeboxComponent>(ent.Owner, out var jukebox))
-        {
-            StopAudio(ent.Owner, jukebox);
-        }
+        if (!TryComp<JukeboxComponent>(ent.Owner, out var jukebox))
+            return;
+
+        StopAudio(ent.Owner, jukebox);
+        CloseUI(ent.Owner);
     }
 
-    private void StopAudio(EntityUid entity, JukeboxComponent jukebox)
+    private void CloseUI(EntityUid ent)
     {
-        if (jukebox.AudioStream != null && Exists(jukebox.AudioStream.Value) && HasComp<MetaDataComponent>(jukebox.AudioStream.Value))
+        if (!TryComp<UserInterfaceComponent>(ent, out var ui))
+            return;
+
+        _ui.CloseUis((ent, ui));
+    }
+
+    private void StopAudio(EntityUid ent, JukeboxComponent jukebox)
+    {
+        if (jukebox.AudioStream != null
+            && Exists(jukebox.AudioStream.Value)
+            && HasComp<MetaDataComponent>(jukebox.AudioStream.Value))
         {
             _audio.SetState(jukebox.AudioStream, AudioState.Stopped);
         }
 
-        Dirty(entity, jukebox);
+        Dirty(ent, jukebox);
+    }
+
+    private void UpdateVisual(EntityUid ent, bool hasPower = false)
+    {
+        // if (!TryComp<AppearanceComponent>(ent, out var appearance))
+        //     return;
+        //
+        // _appearanceSystem.SetData(ent, PoweredPlayerVisualLayers.Powered, hasPower, appearance);
     }
 
     private void OnInit(EntityUid uid, MusicPlayerPortableComponent component, ComponentInit args)
@@ -53,81 +73,4 @@ public sealed class MusicPlayerPortableSystem : EntitySystem
         // UpdatePlayerState(uid);
     }
 
-    // public override void Update(float frameTime)
-    // {
-    //     base.Update(frameTime);
-    //     _nextUpdate -= frameTime;
-    //     if (_nextUpdate > 0)
-    //         return;
-    //     _nextUpdate = UpdateInterval;
-    //
-    //     // Перебираем все сущности с нашим компонентом и JukeboxComponent
-    //     var query = EntityQueryEnumerator<MusicPlayerPortableComponent, JukeboxComponent>();
-    //     while (query.MoveNext(out var uid, out _, out var jukebox))
-    //     {
-    //         UpdatePlayerState(uid, jukebox);
-    //     }
-    // }
-
-    // private void UpdatePlayerState(EntityUid uid, JukeboxComponent? jukebox = null)
-    // {
-    //     if (!Resolve(uid, ref jukebox))
-    //         return;
-    //
-    //     bool hasPower = HasSufficientBatteryPower(uid);
-    //
-    //     // Обновляем визуальное состояние слоя, который определён в прототипе как PowerDeviceVisualLayers.Powered
-    //     _appearanceSystem.SetData(uid, PowerDeviceVisualLayers.Powered, hasPower);
-    //
-    //     // Если питание пропало, а музыка играет – останавливаем через стандартное сообщение
-    //     if (!hasPower && jukebox.Playing)
-    //     {
-    //         // Отправляем локальное сообщение, которое JukeboxSystem обработает
-    //         var stopMsg = new JukeboxStopMessage();
-    //         RaiseLocalEvent(uid, stopMsg);
-    //     }
-    // }
-
-    // private bool HasSufficientBatteryPower(EntityUid uid)
-    // {
-    //     if (!TryComp<PowerCellSlotComponent>(uid, out var slot))
-    //         return false;
-    //
-    //     var cell = GetCellFromSlot(slot);
-    //     if (cell == null)
-    //         return false;
-    //
-    //     if (!TryComp<BatteryComponent>(cell.Value, out var battery))
-    //         return false;
-    //
-    //     // Считаем, что питание есть, если заряд строго больше нуля
-    //     return battery.CurrentCharge > 0;
-    // }
-    //
-    // private EntityUid? GetCellFromSlot(PowerCellSlotComponent slot)
-    // {
-    //     // Попытка получить батарейку из слота разными способами
-    //     // (универсальный подход, т.к. точное поле может отличаться в вашей версии)
-    //
-    //     // Способ 1: прямое поле Cell
-    //     var cellProp = slot.GetType().GetProperty("Cell");
-    //     if (cellProp != null)
-    //         return (EntityUid?)cellProp.GetValue(slot);
-    //
-    //     // Способ 2: контейнер Slot
-    //     var slotProp = slot.GetType().GetProperty("Slot");
-    //     if (slotProp != null)
-    //     {
-    //         var container = (ContainerSlot?)slotProp.GetValue(slot);
-    //         if (container != null && container.ContainedEntity != null)
-    //             return container.ContainedEntity.Value;
-    //     }
-    //
-    //     // Способ 3: свойство ContainedEntity
-    //     var containedProp = slot.GetType().GetProperty("ContainedEntity");
-    //     if (containedProp != null)
-    //         return (EntityUid?)containedProp.GetValue(slot);
-    //
-    //     return null;
-    // }
 }
